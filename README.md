570f4300c1b4feea8231a2ce8b8355076e2c7518
Your Name <you@example.com>
2020-12-17 22:53:57 -0500

    001

---
+++ README.md
@@ -0,0 +1,26 @@
+# @Stocpoolt
+# cajero-frontend
+
+# 2.
+# Continuación Dec13
+
+• Instalaciones necesarias (En Dec)
+cd cajero-frontend
+npm run dev
+
+• (En Dec)
+npm run build
+npm run build --report
+
+• Para correr
+npm run start
+
+• (En Dec)
+npm install --save axios
+npm install --save vue-router
+
+# Crear - router.js -
+# Editar main.js
+# Crear User.vue
+# Crear UserBalance.vue
+# Editar App.vue
---
+++ src/components/User.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="User">
+        <h2>Hi <span> {{username}}, </span>  Welcome!</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "User",
+        data:function(){
+            return {
+                username: "none"
+            }
+        },
+        created: function() {
+            this.username = this.$route.params.username
+        }
+    }
+</script>
+
+<style>
+    #User{
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    #User h2{
+        font-size: 50px;
+        color: #284737;
+    }
+    #User span{
+        color: rgb(21, 13, 95);
+        font-weight: bold;
+    }
+</style>
